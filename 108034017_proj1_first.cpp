b5b4a7b73ae626e1bb420bf9a87fe5bd807bc476
Goosiping <georgekuo100@gmail.com>
2020-09-29 19:43:33 +0800

    Failed to Build Shape

--- 108034017_proj1_first.cpp
+++ 108034017_proj1_first.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 #include<ostream>
 using namespace std;
+/*class t1{
+    public:
+        int shape[2][3];
+        t1(){shape={{1, 1, 1},{0, 1, 0}};
+};*/
 int main(){
-    cout<<"Success"<<endl;
-    cout<<"Revised"<<endl;
+    int col, row;
+    //t1 a;
+    int shape[2][3] = {{1, 1, 1},{0, 1, 0}};
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++){
+            cout<<shape[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
